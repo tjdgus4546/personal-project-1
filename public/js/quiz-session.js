a75dc4183ae4755c5d7ee65d920773a773e4c06b
SeongHyeon Lee <tjdgus4546@naver.com>
2025-10-17 01:36:07 +0900

    초대코드 복사버튼 색상 변경

--- public/js/quiz-session.js
+++ public/js/quiz-session.js
@@ -1640,7 +1640,7 @@ async function copyInviteCode() {
     copyIcon.innerHTML = `
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
     `;
-    copyBtn.classList.remove('bg-blue-500', 'hover:bg-blue-600', 'hover:to-blue-600');
+    copyBtn.classList.remove('bg-[#8BA2FA]', 'hover:bg-[#617DE9]', 'hover:to-blue-600');
     copyBtn.classList.add('bg-green-500', 'hover:bg-green-600');
     
     // 2초 후 원래 상태로 복구
@@ -1650,7 +1650,7 @@ async function copyInviteCode() {
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
       `;
       copyBtn.classList.remove('bg-green-500', 'hover:bg-green-600');
-      copyBtn.classList.add('bg-blue-500', 'hover:bg-blue-600', 'hover:to-blue-600');
+      copyBtn.classList.add('bg-[#8BA2FA]', 'hover:bg-[#617DE9]');
     }, 2000);
     
   } catch (err) {
