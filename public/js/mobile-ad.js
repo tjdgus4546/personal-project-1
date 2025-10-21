38a929a9403f9e8b8e7c6016612f7b36be46ce82
SeongHyeon Lee <tjdgus4546@naver.com>
2025-10-22 00:04:05 +0900

    모바일 광고 페이지 css 수정

--- public/js/mobile-ad.js
+++ public/js/mobile-ad.js
@@ -13,4 +13,15 @@ export async function renderMobileAd() {
 
     // body 맨 앞에 삽입 (navbar 다음)
     document.body.insertAdjacentHTML('afterbegin', adHTML);
+
+    // 카카오 애드핏 스크립트 재실행
+    // 동적으로 추가된 광고 영역도 렌더링되도록 강제 실행
+    try {
+        if (window.adfit) {
+            window.adfit.render();
+        }
+    } catch (error) {
+        // 광고 로드 실패는 조용히 무시 (사용자 경험에 영향 없음)
+        console.debug('Ad render skipped:', error);
+    }
 }
